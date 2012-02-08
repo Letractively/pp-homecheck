6ee2d93f4b93aca67d1fcc39e95cad4b0625ee7b
Ruben Martinez <rmartin@bowdoin.edu>
2012-02-08 14:47:02 -0500

    Added constructor getter functions to  participant entry, updated its test file.

--- domain/ParticipantEntry.php
+++ domain/ParticipantEntry.php
@@ -1,9 +1,31 @@
 <?php
 class ParticipantEntry {
-	private $id;    	// Unique identifier for this entry: yy-mm-dd.participant_id
+	private $id;    	// Unique identifier for this entry: yy-mm-dd . participant_id
 	private $call_time;	// time of day that the participant called in
 	private $result; 	// рс, рHс, рCс, or рDс if normal, Had to call, 
 						// called Contact, or called police Dispatcher, resp.
 	private $note; 		// note associated with this participant on this day
+	
+	//Constructor Function
+	function __construct($date, $participant_id, $call_time, $result, $note) {
+		$this->id = $date . $participant_id;
+		$this->call_time = $call_time;
+		$this->result = $result;
+		$this->note = $note;
+	}
+	//Getter Functions
+	function get_id() {
+		return $this->id;
+	}
+	function get_call_time() {
+		return $this->call_time;
+	}
+	function get_result() {
+		return $this->result;
+	}
+	function get_note() {
+		return $this->note;
+	}
+	
 }
 ?>
--- tests/testParticipantEntry.php
+++ tests/testParticipantEntry.php
@@ -2,7 +2,14 @@
 include_once(dirname(__FILE__).'/../domain/ParticipantEntry.php');
 class testParticipantEntry extends UnitTestCase {
     function testParticipantEntryModule() {     
+    			 //fake participant entry to test
+                 $participantA = new ParticipantEntry("12-02-29", "John.Smith", "9:57", "", "Good worker!");
                  
+                 //testing getter functions
+                 $this->assertTrue($participantA->get_id() == "12-02-29John.Smith");
+                 $this->assertTrue($participantA->get_call_time() == "9:57");
+                 $this->assertTrue($participantA->get_result() == "");
+                 $this->assertTrue($participantA->get_note() == "Good worker!");
     }
 }
 
