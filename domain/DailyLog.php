9288f4192885f096225974953339cc7626ed95ee
Ruben Martinez <rmartin@bowdoin.edu>
2012-02-08 14:47:02 -0500

    Added constructor getter functions to  participant entry, updated its test file.

---
+++ domain/DailyLog.php
@@ -0,0 +1,9 @@
+<?php 
+class DailyLog {
+	private $id;     		// Unique identifier for this log: yy-mm-dd
+	private $entry_ids;	// array of participant entry ids for this date
+	private $volunteer_id;	// id of the volunteer making these entries
+	private $note;		// general note for this day 
+	
+}
+?>
---
+++ domain/Participant.php
@@ -0,0 +1,160 @@
+<?php
+/*
+* Copyright 2012 by Alex Edison, Nicole Erkis, Ruben Martinez, and Allen
+* Tucker.  This program is part of Homecheck, which is free software.  It comes
+* with absolutely no warranty.  You can redistribute and/or modify it under the
+* terms of the GNU Public License as published by the Free Software Foundation
+* (see <http://www.gnu.org/licenses/).
+*/
+
+/*
+* Volunteer class for Homecheck
+* @version February 6, 2012
+*/
+class Participant {
+	private $person_id; 		// id (unique key) = first_name . phone1
+	private $first_name;   		// first name as a string
+	private $last_name;    		// last name as a string
+	private $address;      		// address - string
+	private $city; 	     		// city - string
+	private $state; 	   		// state - string
+	private $zip; 	     		// zip code - integer
+	private $phone1; 	    	// primary phone
+	private $phone2; 	    	// alternate phone
+    private $email; 	    	// email address
+	private $lives_alone;  		// ÒyesÓ or ÒnoÓ with name & relationship of housemate
+	private $contacts;			// array of emergency contacts: 
+								// Òname, relationship, phoneÓ
+	private $physician;			// primary care physician and phone
+	private $lifeline;			// ÒyesÓ, ÒnoÓ, or Òother typeÓ
+	private $in_home_services; 	// array of in-home services being used
+	private $special_needs;		// array of special needs that Volunteers should know
+	private $hidden_key;		// ÒnoÓ or Òyes Ð give locationÓ
+	private $other_key; 		// Òname, address, and phoneÓ of other person with a key
+	private $has_car;			// ÒnoÓ or Òyes Ð give license plate and descriptionÓ
+	private $birthday;    		// format: yy-mm-dd
+	private $background_check; 	// ÒauthorizedÓ, ÒunauthorizedÓ, ÒcompletedÓ
+	private $start_date;   		// format: yy-mm-dd
+	private $end_date;			// format: yy-mm-dd
+	private $status;       		// "active", "away", or "former"	
+	private $log_entries;  		// array of LogEntrys for this person
+	private $notes;				// general notes about this person
+	
+	/**
+	* constructor for a Participant
+	*/
+	function __construct($last_name, $first_name, $address, $city, $state, $zip, $phone1, $phone2, $email, 
+	$lives_alone, $contacts, $physician, $lifeline, $in_home_services, $special_needs, $hidden_key, $other_key, 
+	$has_car, $birthday, $background_check, $start_date, $end_date, $status, $log_entries, $notes) {
+		$this->person_id = $first_name . $phone1;
+		$this->last_name = $last_name;
+		$this->first_name = $first_name;
+		$this->address = $address;
+		$this->city = $city;
+		$this->state = $state;
+		$this->zip = $zip;
+		$this->phone1 = $phone1;
+		$this->phone2 = $phone2;
+		$this->email = $email;
+		$this->lives_alone = $lives_alone;
+		$this->contacts = explode(',',$contacts);
+		$this->physician = $physician;
+		$this->lifeline = $lifeline;
+		$this->in_home_services = explode(',',$in_home_services);
+		$this->special_needs = explode(',',$special_needs);
+		$this->hidden_key = $hidden_key;
+		$this->other_key = $other_key;
+		$this->has_car = $has_car;
+		$this->birthday = $birthday;
+		$this->background_check = $background_check;
+		 
+		$this->start_date = $start_date;
+		$this->end_date = $end_date;
+		$this->status = $status;
+		$this->log_entries = explode(',',$log_entries);
+		$this->notes = $notes;
+	}
+	
+	//getter functions
+	function get_person_id() {
+		return $this->person_id;
+	}
+	function get_first_name() {
+		return $this->first_name;
+	}
+	function get_last_name() {
+		return $this->last_name;
+	}
+	function get_address() {
+		return $this->address;
+	}
+	function get_city() {
+		return $this->city;
+	}
+	function get_state() {
+		return $this->state;
+	}
+	function get_zip() {
+		return $this->zip;
+	}
+	function get_phone1() {
+		return $this->phone1;
+	}
+	function get_phone2() {
+		return $this->phone2;
+	}
+	function get_email() {
+		return $this->email;
+	}
+	function get_lives_alone() {
+		return $this->lives_alone;
+	}
+	function get_contacts() {
+		return $this->contacts;
+	}
+	function get_physician() {
+		return $this->physician;
+	}
+	function get_lifeline() {
+		return $this->lifeline;
+	}
+	function get_in_home_services() {
+		return $this->in_home_services;
+	}
+	function get_special_needs() {
+		return $this->special_needs;
+	}
+	function get_hidden_key() {
+		return $this->hidden_key;
+	}
+	function get_other_key() {
+		return $this->other_key;
+	}
+	function get_has_car() {
+		return $this->has_car;
+	}
+	function get_birthday() {
+		return $this->birthday;
+	}
+	function get_background_check() {
+		return $this->background_check;
+	}
+	function get_start_date() {
+		return $this->start_date;
+	}
+	function get_end_date() {
+		return $this->end_date;
+	}
+	function get_status() {
+		return $this->status;
+	}
+	function get_log_entries() {
+		return $this->log_entries;
+	}
+	function get_notes() {
+		return $this->notes;
+	}
+	
+	//setters later
+}
+?>
