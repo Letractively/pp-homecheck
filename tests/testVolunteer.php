53d479faaa9d1c803d1d788b25aec703a61cc323
allen <allen@localhost>
2012-02-11 14:49:39 -0500

    fixed bug in array argument constructor

--- tests/testVolunteer.php
+++ tests/testVolunteer.php
@@ -5,7 +5,7 @@ class testVolunteer extends UnitTestCase {
              
         //fake person to test
         $volunteer = new Volunteer("Smith", "John", "14 Way St","Harpswell", "ME", "04079", "(207)111-2345", "", 
-    				"jsmith@aol.com", "contact1:2071112222:c1@email.com,contact2:2072223333:c2@email.com",
+    				"jsmith@aol.com", "Mary:2071112222:Mary@email.com,Sue:2072223333:Sue@email.com",
         			"retired", "", "","completed", "Wed:3,Fri:4,FI","Wed:3","",
     				"08-01-01","", "active", "", "");
                  
@@ -21,13 +21,13 @@ class testVolunteer extends UnitTestCase {
         $this->assertTrue($volunteer->get_email() == "jsmith@aol.com");
         // testing each individual contact
         $contacts = $volunteer->get_contacts();
-        $this->assertEqual($contacts[0], "contact1:2071112222:c1@email.com");
-        $this->assertEqual($contacts[1], "contact2:2072223333:c2@email.com");
+        $this->assertEqual($contacts[0], "Mary:2071112222:Mary@email.com");
+        $this->assertEqual($contacts[1], "Sue:2072223333:Sue@email.com");
         // testing first contact's name, phone, and email
         $first_contact = explode(':',$contacts[0]);
-        $this->assertEqual($first_contact[0], "contact1");
+        $this->assertEqual($first_contact[0], "Mary");
         $this->assertEqual($first_contact[1], "2071112222");
-        $this->assertEqual($first_contact[2], "c1@email.com");
+        $this->assertEqual($first_contact[2], "Mary@email.com");
                  
         echo ("testVolunteer complete\n");
     }
