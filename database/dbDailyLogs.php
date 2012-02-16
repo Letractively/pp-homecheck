86ce8d0846f77cf64d2603662da92c7dd79179e3
Alex Edison <edison.ace@gmail.com>
2012-02-16 16:02:34 -0500

    Added the first framework for some of the database code, as well as dbinfo.php.  Everything except dbinfo.php is currently empty.

---
+++ database/dbDailyLogs.php
@@ -0,0 +1,42 @@
+<?php
+/*
+* Copyright 2012 by Alex Edison, Nicole Erkis, Ruben Martinez, and Allen
+* Tucker.  This program is part of Homecheck, which is free software.  It comes
+* with absolutely no warranty.  You can redistribute and/or modify it under the
+* terms of the GNU Public License as published by the Free Software Foundation
+* (see <http://www.gnu.org/licenses/).
+*/
+
+/*
+* Volunteer class for Homecheck
+* @author Alex Edison
+* @version February 16, 2012
+*/
+
+include_once(dirname(__FILE__).'/../domain/DailyLog.php');
+include_once(dirname(__FILE__).'/dbinfo.php');
+
+function create_dbDailyLog(){
+	
+}
+
+function retreive_dbDailyLog($id){
+	
+}
+
+function getall_dbDailyLog(){
+	
+}
+
+function insert_dbDailyLog($dailyLog){
+	
+}
+
+function update_dbDailyLog($dailyLog){
+	
+}
+
+function delete_dbDailyLog($id){
+	
+}
+?>
---
+++ database/dbVolunteers.php
@@ -0,0 +1,42 @@
+<?php
+/*
+* Copyright 2012 by Alex Edison, Nicole Erkis, Ruben Martinez, and Allen
+* Tucker.  This program is part of Homecheck, which is free software.  It comes
+* with absolutely no warranty.  You can redistribute and/or modify it under the
+* terms of the GNU Public License as published by the Free Software Foundation
+* (see <http://www.gnu.org/licenses/).
+*/
+
+/*
+* Volunteer class for Homecheck
+* @author Alex Edison
+* @version February 16, 2012
+*/
+
+include_once(dirname(__FILE__).'/../domain/Vounteer.php');
+include_once(dirname(__FILE__).'/dbinfo.php');
+
+function create_dbVolunteers(){
+	
+}
+
+function retreive_dbVolunteers($id){
+	
+}
+
+function getall_dbVolunteers(){
+	
+}
+
+function insert_dbVolunteers($volunteer){
+	
+}
+
+function update_dbVolunteers($person){
+	
+}
+
+function delete_dbVolunteers($id){
+	
+}
+?>
