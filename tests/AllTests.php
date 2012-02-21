97ed7fdcdb399b466e793001b84f183afbdfc4ca
Alex Edison <edison.ace@gmail.com>
2012-02-21 01:56:49 -0500

    Finished dbVolunteers and added testdbVolunteers.  Have not completed running tests yet, as something has gone haywire with my simpletest.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -5,6 +5,7 @@
 // require_once(dirname(__FILE__).'/simpletest/autorun.php');
 class AllTests extends GroupTest {
  	  function AllTests() {
+ 	  	$this->addTestFile(dirname(__FILE__).'/testdbVolunteers.php');
         $this->addTestFile(dirname(__FILE__).'/testVolunteer.php');
         $this->addTestFile(dirname(__FILE__).'/testParticipant.php');
         $this->addTestFile(dirname(__FILE__).'/testDailyLog.php');
@@ -12,7 +13,9 @@ class AllTests extends GroupTest {
         $this->addTestFile(dirname(__FILE__).'/testParticipantEntry.php');
         $this->addTestFile(dirname(__FILE__).'/testReport.php');
 		$this->addTestFile(dirname(__FILE__).'/testShift.php');
-		$this->addTestFile(dirname(__FILE__).'/testScheduleEntry.php');        
+		$this->addTestFile(dirname(__FILE__).'/testScheduleEntry.php');
+		
+		
         echo ("All tests complete");
  	  }
  }
