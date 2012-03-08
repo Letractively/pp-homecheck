13fb1965b396107565d511ff1496531b878eeb5f
rmartin <rmartin@bowdoin.edu>
2012-03-08 00:11:36 -0500

    I've included the DailyLog page I am working on. It is close to being fully functional in terms of retrieving participants from dbParticipants and posting to the dbParticipantEntry. Note that some of the authentication details are specific to my own phpmyadmin database, and would have to be updated for the pp-homecheck database. Other than that, everything should be fully functional

---
+++ DailyLog/writeToDL.php
@@ -0,0 +1,21 @@
+<?php
+$con = mysql_connect("127.0.0.1","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("pphomecheckdb");
+
+$sql="INSERT INTO dbParticipantEntry(date, id, call_time, result, note)
+VALUES
+('9122012','$_POST[Participant]','$_POST[Time]','$_POST[Result]','$_POST[Notes]')";
+
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+
+header("Location: http://127.0.0.1/pp-homecheck/DailyLog/dailylog.php");
+mysql_close($con)
+?>
