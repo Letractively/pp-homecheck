5b3b1ba4a4ec57d59fc031b94194694db8668a06
rmartin <rmartin@bowdoin.edu>
2012-04-03 23:05:41 -0400

    added new DailyLog, works almsot entirely except for ListBox

--- writeToDL.php
+++ writeToDL.php
@@ -1,21 +1,17 @@
 <?php
-$con = mysql_connect("127.0.0.1","root","");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
+	$con = mysql_connect("127.0.0.1","root","");
+	if (!$con) {
+  		die('Could not connect: ' . mysql_error());
+  	}
 
-mysql_select_db("pphomecheckdb");
+	mysql_select_db("pphomecheckdb");
+	
+	$sql="INSERT INTO dbParticipantEntry(date, id, call_time, result, note) 
+		  VALUES('$_POST[Date]','$_POST[Participant]','$_POST[Time]','$_POST[Result]','$_POST[Notes]')";
+	if (!mysql_query($sql,$con)) {
+  		die('Error: ' . mysql_error());
+  	}
 
-$sql="INSERT INTO dbParticipantEntry(date, id, call_time, result, note)
-VALUES
-('9122012','$_POST[Participant]','$_POST[Time]','$_POST[Result]','$_POST[Notes]')";
-
-if (!mysql_query($sql,$con))
-  {
-  die('Error: ' . mysql_error());
-  }
-
-header("Location: http://127.0.0.1/pp-homecheck/DailyLog/dailylog.php");
-mysql_close($con)
+	header("Location: http://127.0.0.1/pp-homecheck/viewDailyLog.php");
+	mysql_close($con)
 ?>
